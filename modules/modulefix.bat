ce8061c4769c2bf1cd1c5c053a7e1ecd8bd5dc1d
Vaquent2 <145298885+Vaquent2@users.noreply.github.com>
2024-09-26 20:48:01 -0500

    Update modulefix.bat

--- modules/modulefix.bat
+++ modules/modulefix.bat
@@ -2,6 +2,6 @@
 echo Windows Registry Editor Version 5.00 > increase_paging_file.reg
 echo. >> increase_paging_file.reg
 echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management] >> increase_paging_file.reg
-echo "PagingFiles"="C:\pagefile.sys 17408 17408" >> increase_paging_file.reg
+echo "PagingFiles"="C:\pagefile.sys 30000 30000" >> increase_paging_file.reg
 regedit /s increase_paging_file.reg
 del increase_paging_file.reg

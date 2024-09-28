a1a59ee51953db54200c8f8d96365058f7b03e9b
Vaquent2 <145298885+Vaquent2@users.noreply.github.com>
2024-09-28 23:08:26 +0000

    run as admin (recoded)

--- potentionalmodulefix.bat
+++ potentionalmodulefix.bat
@@ -3,10 +3,10 @@
 echo Windows Registry Editor Version 5.00 > increase_paging_file.reg
 echo. >> increase_paging_file.reg
 echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management] >> increase_paging_file.reg
-echo "PagingFiles"="C:\pagefile.sys 30000 30000" >> increase_paging_file.reg
+echo "PagingFiles"="C:\pagefile.sys 50000 50000" >> increase_paging_file.reg
 regedit /s increase_paging_file.reg
 del increase_paging_file.reg
 
 :: Alternative method using WMIC
 wmic COMPUTERSYSTEM set AutomaticManagedPagefile=false
-wmic PAGEFILESET set InitialSize=12000,MaximumSize=32000
+wmic PAGEFILESET set InitialSize=50000,MaximumSize=50000

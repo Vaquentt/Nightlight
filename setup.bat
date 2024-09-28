81557aa8388856630feaea9547c4d1a584c82096
Vaquent2 <145298885+Vaquent2@users.noreply.github.com>
2024-09-28 08:12:40 -0500

    python 3.8-11 auto download

--- setup.bat
+++ setup.bat
@@ -2,12 +2,12 @@
 color 5
 
 title Checking Python Version
-python --version | findstr "3.11."
+python --version | findstr "3.11." || python --version | findstr "3.10." || python --version | findstr "3.9." || python --version | findstr "3.8."
 if %errorlevel%==0 (
-    echo Python 3.11.0 or higher is already installed
+    echo Python 3.8.0 or higher is already installed
 ) else (
-    echo Python 3.11.0 or higher is not installed
-    echo Please install Python 3.11.0 or higher and add it to your PATH
+    echo Python 3.8.0 or higher is not installed
+    echo Please install Python 3.8.0 or higher and add it to your PATH
     timeout /t 5 >nul
 )
 
